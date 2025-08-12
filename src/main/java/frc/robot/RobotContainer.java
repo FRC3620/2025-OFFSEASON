d83e2f3c366c2af42122f77158d997103321da52
Greg Fisbeck <greg@fisbeck.com>
2025-08-12 19:28:28 -0400

    Added zeroGyro button (A)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,8 +27,10 @@ import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import swervelib.SwerveDrive;
 import swervelib.SwerveInputStream;
 import swervelib.parser.json.modules.DriveConversionFactorsJson;
 
@@ -79,6 +81,7 @@ public class RobotContainer {
     driverJoystick = new ChameleonController(new Joystick(0));
 
     //TODO: NavX Reset
+    driverJoystick.button(XBoxConstants.BUTTON_A, FlySkyConstants.BUTTON_SWA).onTrue(new InstantCommand(() -> swerveSubsystem.zeroGyro()));
     
   }
 
